01de79b9ca995dd4107cfc2f06e45c39539d54ba
AMIR101001 <mulikov2012@mail.ru>
2022-11-16 22:34:40 +0300

    Добить класс PointService и составить тесты к нему

--- src/main/java/task/point/PointService.java
+++ src/main/java/task/point/PointService.java
@@ -6,4 +6,7 @@ public class PointService {
         return Math.sqrt((Math.pow(firstPointX, 2) - Math.pow(firstPointY, 2)) + (Math.pow(secondPointX, 2) - Math.pow(secondPointY, 2)));
 
     }
+    public double distanceBetweenPoints(int a, int b){
+            return Math.abs(a) - Math.abs(b);
+    }
 }
